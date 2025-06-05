46616946320703796f6ed3566c77286d49345cf1
nataliamd11 <nataliamdenk11@gmail.com>
2025-06-05 10:39:03 -0300

    feat: change database config to postgres db

--- djangopg/djangopg/settings.py
+++ djangopg/djangopg/settings.py
@@ -72,10 +72,16 @@ WSGI_APPLICATION = 'djangopg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': os.getenv('POSTGRES_DB'),
+       'USER': os.getenv('POSTGRES_USER'),
+       'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+       'HOST': os.getenv('PG_HOST'),
+       'PORT': os.getenv('PG_PORT'),
     }
 }
 
